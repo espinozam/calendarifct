db9b90d72b6592e482ccd6314866e15e91535476
Edwin Espinoza <edwin.smx2@gmail.com>
2026-01-10 10:58:48 +0100

    Añadido formulario con validaciones y valores por defecto

--- src/app/components/formulari-practiques/formulari-practiques.ts
+++ src/app/components/formulari-practiques/formulari-practiques.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulari-practiques',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './formulari-practiques.html',
   styleUrl: './formulari-practiques.css',
 })
 export class FormulariPractiques {
+
+  // formulario
+  formulari: FormGroup;
+
+  // constructor
+  constructor(private fb: FormBuilder) {
+
+    // creacion del formulario reactivo
+    // propiedad: [valor por defecto, validaciones]
+    // ejemplo: horesDiaries, valor por defecto = 8, valor minimo = 1, valor maximo = 8
+    this.formulari = this.fb.group({
+      dataInici: ['', Validators.required],
+      horesTotals: [400, [Validators.required, Validators.min(1)]],
+      horesDiaries: [8, [Validators.required, Validators.min(1), Validators.max(8)]]
+    });
+  }
+
+  // funcion enviar
+  enviar() {
+    // comprobar si los campos del formulario son validos
+    if (this.formulari.valid) {
+      // imprime en consola los datos del formulario
+      console.log(this.formulari.value);
+    } else {
+      // muestra los errores de validacion en el formulario
+      this.formulari.markAllAsTouched();
+    }
+  }
 
 }
